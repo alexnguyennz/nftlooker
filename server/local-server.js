b82a37fbe3b685d15781de5103665c668b4f6bb0
Alex Nguyen <andevnetnz@gmail.com>
2022-02-20 17:59:50 +1300

    lambda

--- server/local-server.js
+++ server/local-server.js
@@ -1,15 +1,5 @@
 const app = require('./functions/server.js');
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-app.use(
-  '/.netlify/functions/server',
-  createProxyMiddleware({
-    target: 'https://nftlooker-server.netlify.app',
-    changeOrigin: true,
-  })
-);
-
 const PORT = process.env.PORT || 7777;
 
 app.listen(PORT, () => {
