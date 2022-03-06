edef963ca8240a6e773bf180a5dabdbb4adcee6c
Alex Nguyen <andevnetnz@gmail.com>
2022-03-06 18:50:54 +1300

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'nftlooker',
       script: './src/server.js',
-      instances: '1',
+      instances: 1,
       env: {
         NODE_ENV: 'development',
       },
