2425e7b675eab609916662156d3a9dcecb9e0fa4
gladeth <101024051+gladeth@users.noreply.github.com>
2022-04-06 15:38:24 +1200

    go
    - refactoring

--- go/server.go
+++ go/server.go
@@ -11,341 +11,211 @@ import (
 	"strings"
 	"sync" // return errors to the caller
 
-	"github.com/gorilla/mux"
+	"os"
+
+	"github.com/gorilla/mux" // Router
+	"github.com/joho/godotenv"
 )
 
-// `${process.env.MORALIS_API_URL}/${resolvedAddress}/nft?chain=${chain}&format=decimal`,
-// https://deep-index.moralis.io/api/v2
-// `/api/nfts?chain=${chain}&address=${params.walletAddress}`,
+// TYPES
+type Result struct {
+	Token_Address string `json:"token_address"`
+	Token_Id string `json:"token_id"`
+	Block_Number_Minted string `json:"block_number_minted"`
+	Owner_Of string `json:"owner_of"`
+	Block_Number string `json:"block_number"`
+	Amount string `json:"amount"`
+	Contract_Type string `json:"contract_type"`
+	Name string `json:"name"`
+	Symbol string `json:"symbol"`
+	Token_Uri string `json:"token_uri"`
+	Metadata string `json:"metadata"`
+	Synced_At string `json:"synced_at"`
+	Is_Valid int `json:"is_valid"`
+	Syncing int `json:"syncing"`
+	Frozen int `json:"frozen"`
+}
+
+type Response struct {
+	Result []Result `json:"result"`
+}
+
 func getWalletNfts(w http.ResponseWriter, r *http.Request) {
+
+	// PARAMS
 	vars := mux.Vars(r)
 	chain := vars["chain"]
 	address := vars["address"]
 
-	//chain := r.URL.Query().Get("chain")
 
+	/*** REQUEST ***/
+	/***************/
 	
-
-	w.WriteHeader(http.StatusOK)
-  //fmt.Fprintf(w, "Chain: %v\n", chain)
-  //fmt.Fprintf(w, "Address: %v\n", address)
-
-	// resp, err := http.Get("https://deep-index.moralis.io/api/v2/" + address + "/nft?chain=" + chain)
-	//fmt.Fprintf(w, "response", resp, err)
-
-	req, err := http.NewRequest("GET", "https://deep-index.moralis.io/api/v2/" + address + "/nft?chain=" + chain, nil)
+	// Moralis GetNFTs https://github.com/nft-api/nft-api#getnfts
+	req, err := http.NewRequest("GET", os.Getenv("MORALIS_API_URL") + address + "/nft?chain=" + chain, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req.Header.Set("accept", "application/json")
-	req.Header.Set("X-API-KEY", "")
+	// Set Headers
+	req.Header = http.Header{
+		"accept": []string{"application/json"},
+		"x-api-key": []string{os.Getenv("MORALIS_API_KEY")},
+	}
 
-	// make request
+	// Make Request
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Format Body
 	defer resp.Body.Close()
 	resBody, _ := ioutil.ReadAll(resp.Body)
 	response := string(resBody)
-	// end request
+
+	/*** END REQUEST ***/
+	/*******************/
 
 
-	type Result struct {
-		Token_Address string `json:"token_address"`
-		Token_Id string `json:"token_id"`
-		Block_Number_Minted string `json:"block_number_minted"`
-		Owner_Of string `json:"owner_of"`
-		Block_Number string `json:"block_number"`
-		Amount string `json:"amount"`
-		Contract_Type string `json:"contract_type"`
-		Name string `json:"name"`
-		Symbol string `json:"symbol"`
-		Token_Uri string `json:"token_uri"`
-		Metadata string `json:"metadata"`
-		Synced_At string `json:"synced_at"`
-		Is_Valid int `json:"is_valid"`
-		Syncing int `json:"syncing"`
-		Frozen int `json:"frozen"`
-	}
-
-	type Response struct {
-		Result []Result `json:"result"`
-	}
-
+	// Declare Response typed result
 	var result Response
 
 	err = json.Unmarshal([]byte(response), &result)
 
 	if err != nil {
-		fmt.Println("Couldn't unmarshal result", err)
-		
+		fmt.Println("Couldn't unmarshal: ", err)
+		return 
 	}
 	
-	// LOOP
-	// data := &Data{ManyItems: make([]Item, len(resp))}
-	//list := &Response{Result: make([]Result, len(resBody))}
-	//list := make([]Result, len(resp))
+	var wg sync.WaitGroup // Create WaitGroup to wait for all goroutines to finish
 
-	//fmt.Fprintf(w, "List: %v", list)
+	// Loop through each NFT's results
+	for i, nft := range result.Result {
 
-	//fmt.Fprintf(w, "%v\n\n", result)
+		wg.Add(1)
 
-	//fmt.Fprintf(w, "Unmarshalled: \n%v\n\n", response)
+		// Fetch each NFT's metadata in parallel
+		go func(i int, nft Result) {
 
-	//fmt.Fprintf(w, "Original Metadata: \n%v\n\n", result)
+			// Decrease WaitGroup when goroutine has finished
+			defer wg.Done()
 
-	updatedResult := result
-	
-	//fmt.Fprintf(w, "test %v", updatedResult.Result[0].Metadata)
-
-	for _, nft := range result.Result {
-		//fmt.Fprintf(w, "%s\n", nft.Token_Uri)
-
-		//fmt.Fprintf(w, "Index: %v \n%v\n\n", i, nft)
-
-		var waitgroup sync.WaitGroup
-		waitgroup.Add(1)
-
-
-
-		// var metadata string
-
-		//var metadata Metadata
-		var metadata map[string]interface{}
-
-		// fetch token_uri in parallel
-		go func() {
 			response, err := Request(nft.Token_Uri)
 
 			if err != nil {
 				fmt.Println("Error -", err)
 				//w.WriteHeader(http.StatusInternalServerError)
-				// waitgroup.Done()
-				// return
+				return
 			}
 
-			//fmt.Fprintf(w, "Token Uri Response: \n%s\n\n", response)
+			var metadata map[string]interface{}
+
+			err = json.Unmarshal([]byte(response), &metadata)
+
+			if err != nil {
+				fmt.Println("Couldn't unmarshal", err)
+			}
 			
-			errs := json.Unmarshal([]byte(response), &metadata)
+			// changeIpfsUrl
+			if metadata != nil {
 
-			if errs != nil {
-				fmt.Println("error test")
-			}
-			
-			//fmt.Fprintf(w, "%s", metadata)
-			waitgroup.Done() // decrease waitgroup once goroutine has finished
-		} ()
-
-		// block until waitgroup becomes 0 or all goroutines are done
-		waitgroup.Wait()
-
-		//fmt.Println("test", )
-
-		
-
-
-		// changeIpfsUrl
-		//if metadata["Image"] != "" {
-		if metadata != nil {	// metadata must exist 
-			//fmt.Println("metadata", metadata)
-
-			// some NFTs use image_url, set it back to image
-			if metadata["image_url"] != nil {
-				metadata["image"] = metadata["image_url"]
-			}
-
-			if metadata["image"] != nil {
-				if strings.HasPrefix(metadata["image"].(string), "ipfs://ipfs/") {
-
-					u, _ := url.Parse(metadata["image"].(string))
-					u.Scheme = "https"
-					u.Path = "/ipfs" + u.Path // prepend /ipfs/ path to CID
-					u.Host = "ipfs.io"
-	
-					//metadata["Image"] = u.String()
-					
-				} else if strings.HasPrefix(metadata["image"].(string), "ipfs://") {
-	
-					u, _ := url.Parse(metadata["image"].(string))
-					u.Scheme = "https"
-					u.Path = "/ipfs/" + u.Host // prepend /ipfs/ path to CID
-					u.Host = "ipfs.io"
-	
-					metadata["image"] = u.String()
-	
-				} else if strings.HasPrefix(metadata["image"].(string), "https://gateway.pinata.cloud/") {
-	
-					u, _ := url.Parse(metadata["image"].(string))
-					u.Host = "ipfs.io"
-	
-					metadata["image"] = u.String()
-					// TESTING
-				} else {
-	
-					
-					u, err := url.Parse(metadata["image"].(string))
-	
-					if err != nil {
-						fmt.Println("test", err)
-					}
-					u.Host = "test.io"
-	
-					metadata["image"] = u.String()
+				// some NFTs use image_url, set it back to image
+				if metadata["image_url"] != nil {
+					metadata["image"] = metadata["image_url"]
 				}
-	
-				// marshalled JSON data after updating the metadata
-				//fmt.Fprintf(w, "Unmarshalled: %v\n\n", metadata)
-				updatedData, _ := json.Marshal(metadata)
-	
-				// update existing metadata with new
-				nft.Metadata = string(updatedData)
 
-				//fmt.Fprintf(w, "Updated metadata %v", metadata)
-	
-				// old method
-				//updatedResult.Result[i].Metadata = nft.Metadata
-				
-				// new method
-				//updatedResult.Result[i].Metadata = metadata
+				if metadata["image"] != nil {
 
-				// fmt.Fprintf(w, "%s\n\n", nft.Metadata)
-			}
+					// parse image URL
+					u, _ := url.Parse(metadata["image"].(string))
 
-			
+					if strings.HasPrefix(metadata["image"].(string), "ipfs://ipfs/") {
 
-		}
-
+						u.Scheme = "https"
+						u.Path = "/ipfs" + u.Path // prepend /ipfs/ path to CID
+						u.Host = "ipfs.io"
+						
+					} else if strings.HasPrefix(metadata["image"].(string), "ipfs://") {
 		
-	}
+						u.Scheme = "https"
+						u.Path = "/ipfs/" + u.Host // prepend /ipfs/ path to CID
+						u.Host = "ipfs.io"
+		
+					} else if strings.HasPrefix(metadata["image"].(string), "https://gateway.pinata.cloud/") {
+						u.Host = "ipfs.io"
+					} else { // Testing
+						u.Host = "TESTINGCHANGETHIS.IO"
+					} 
 
-	// updated
-	//fmt.Fprintf(w, "Updated Metadata:\n%v\n\n", updatedResult)
+					metadata["image"] = u.String() // update transformed image URL
+		
+					// Format into JSON
+					metadataJson, _ := json.Marshal(metadata)
+					data := string(metadataJson)
+		
+					result.Result[i].Metadata = data // update original response
+				}
+			}
+		}(i, nft) // End goroutine
+	} // End for range loop
+	
+	// Block execution until all goroutines are done
+	wg.Wait()
 
-
-	/* for i, collection := range updatedResult.Result {
-		fmt.Fprintf(w, "COLLECTION %v\n%v\n\n", i, collection)
-	} */
-
-	// make slice with array index of token_address string and rest of token data
+	// Group NFTs by collection address
 	grouped := make(map[string][]Result)
 
-	for _, collection := range updatedResult.Result {
-		//fmt.Fprintf(w, "test %v\n\n", collection.Token_Address)
+	for _, collection := range result.Result {
 		grouped[collection.Token_Address] = append(grouped[collection.Token_Address], collection)
 	}
 
-	
+	// Format into JSON
+	groupedJson, _ := json.Marshal(grouped)
+	data := string(groupedJson)
 
-	/* for _, collection := range grouped {
-		for i, v := range collection {
-			fmt.Fprintf(w, "COLLECTION %v\n%v\n\n", i, v)
-		}
-		
-	} */
-
-	//fmt.Fprintf(w, "Grouped %v\n\n", grouped)
-
-	// MARSHALLED
-	sendData, _ := json.Marshal(grouped)
-	marshalled := string(sendData)
-
-	fmt.Fprintf(w, "%v", marshalled)
-
-
+	// Send HTTP Response
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%v", data) // Send Data Response 
 
 }
 
-func Request(token_uri string) (string, error) {
-	//start := time.Now()
-	resp, err := http.Get(token_uri)
+func Request(url string) (string, error) {
+	
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Response Error", err)
-		return "", errors.New("couldn't fetch Token URI")
+		return "", errors.New("response error from Get() of " + url)
 	}
+
+	// close once body is returned
+	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Body Error", err)
 		return "", errors.New("couldn't read body")
 	}
 
+	data := string(body)
 
-	response := string(body)
-
-	// fmt.Printf("%v", response)
-
-	return response, nil
-
+	return data, nil
 }
-
-// formatting
-// checks for content type
-
 
 
 func main() {
-	r := mux.NewRouter()
-    // Routes consist of a path and a handler function.
-	r.HandleFunc("/nfts/chain/{chain}/address/{address}", getWalletNfts).Methods("GET")
 
-	log.Fatal(http.ListenAndServe("localhost:9999", r))
+	r := mux.NewRouter() // gorilla/mux router
+
+	// LOAD ENV VARIABLES
+	err := godotenv.Load()
+	if err != nil { log.Fatal("Error loading .env file") }
+	
+	/***** ROUTES *****/
+	/******************/
+
+	r.HandleFunc("/nfts/chain/{chain}/address/{address}", getWalletNfts)
+
+	/*** END ROUTES ***/
+	/******************/
+
+	log.Fatal(http.ListenAndServe("localhost:" + os.Getenv("GO_PORT"), r))
 }
-
-
-
-
-// default
-/* func main() {
-
-	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Printf("Starting server at port 7777\n")
-
-	// handler is typically nil so DefaultServeMux is used
-	//if err := http.ListenAndServe(":7777", nil); err != nil {
-	if err := http.ListenAndServe("127.0.0.1:7777", nil); err != nil {
-		log.Fatal(err)
-	}
-} */
-
-
-		/* get the image's Content-Type
-		resp, _ := http.Head(metadata.Image)
-		contentType := resp.Header.Get("Content-Type")
-		fmt.Fprintf(w, "%s", contentType) */
-
-		//fmt.Fprintf(w, "%s", metadata.Image)
-
-
-					/*
-				// ipfs://ipfs/
-				u, _ := url.Parse("ipfs://ipfs/QmbkBdEkU9WX9fiZwjp4A3JuXrA5XazgdF29ioJcQJxzmz")
-				u.Scheme = "https"
-				u.Path = "/ipfs" + u.Path // prepend /ipfs/ path to CID
-				u.Host = "ipfs.io"
-
-				metadata.Image = u.String()
-
-				fmt.Fprintf(w, "ipfs://ipfs %s\n", metadata.Image)
-
-				// ipfs://
-				u, _ = url.Parse("ipfs://QmbkBdEkU9WX9fiZwjp4A3JuXrA5XazgdF29ioJcQJxzmz")
-				u.Scheme = "https"
-				u.Path = "/ipfs/" + u.Host // prepend /ipfs/ path to CID
-				u.Host = "ipfs.io"
-
-				metadata.Image = u.String()
-
-				fmt.Fprintf(w, "ipfs:// %s\n", metadata.Image)
-
-				// https://gateway.pinata.cloud/
-				u, _ = url.Parse("https://gateway.pinata.cloud/ipfs/QmbkBdEkU9WX9fiZwjp4A3JuXrA5XazgdF29ioJcQJxzmz")
-				u.Host = "ipfs.io"
-
-				metadata.Image = u.String()
-				fmt.Fprintf(w, "https://gateway.pinata.cloud/ %s\n", metadata.Image) */
-
-		
